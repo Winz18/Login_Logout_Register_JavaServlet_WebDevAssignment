0b4f9563a71d19704a75ea3d2d2cba311fe5e3a4
Winz18 <117364628+Winz18@users.noreply.github.com>
2024-09-21 21:06:50 +0700

    add login

--- src/main/java/com/example/baitap1/controller/Login.java
+++ src/main/java/com/example/baitap1/controller/Login.java
@@ -1,4 +1,35 @@
 package com.example.baitap1.controller;
 
-public class Login {
+import com.example.baitap1.model.UserModel;
+import com.example.baitap1.services.implement.UserServicesImp;
+import com.example.baitap1.services.interfaces.UserServiesInt;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/login"})
+public class Login extends HttpServlet {
+    private UserServiesInt userServicesInt = new UserServicesImp();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/views/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        UserModel user = userServicesInt.login(username, password);
+        if (user != null) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user", user);
+            resp.sendRedirect(req.getContextPath() + "/home");
+        } else {
+            req.setAttribute("message", "Username or password is incorrect");
+            req.getRequestDispatcher("/views/login.jsp").forward(req, resp);
+        }
+    }
 }
---
+++ src/main/webapp/views/index.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Assignment_01</title>
+</head>
+<body>
+<h4>Login</h4>
+<a href="/BaiTap1_war_exploded/login">Login</a>
+<h4>Register</h4>
+<a href="/BaiTap1_war_exploded/register">Register</a>
+<h4>Logout</h4>
+<a href="/BaiTap1_war_exploded/logout">Logout</a>
+</body>
+</html>
